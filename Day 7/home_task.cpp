8e46319b1e0318f82038314461fd811043b9f5d4
Milap70 <98155640+Milap70@users.noreply.github.com>
2022-03-03 16:04:28 +0530

    Update home_task.cpp

--- Day 7/home_task.cpp
+++ Day 7/home_task.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 using namespace std;
-
 class Measure{
 
 	public:
@@ -20,7 +19,6 @@ class Measure{
 		return height*length*width;
 	}
 };
-
 int main()
 {
   Measure obj1,obj2;
@@ -28,11 +26,7 @@ int main()
   obj2.setInput(5.5,6,7.99);
   cout<< "Area of object1 " << "height("<< obj1.height << ") "<< "and " << "length("<<obj1.length <<") " <<"is " << obj1.area()<< endl;
   cout<< "Volume of object1" << "height("<< obj1.height <<") "<< "length("<< obj1.length <<") "<< "width("<<obj1.width<<") " << "is " << obj1.volume()<< endl;
-
-  cout<< "Area of object2 " << "height("<< obj2.height<<") " << "and " << "length("<<obj2.length<<") " << "is " << obj2.area()<< endl;
+ cout<< "Area of object2 " << "height("<< obj2.height<<") " << "and " << "length("<<obj2.length<<") " << "is " << obj2.area()<< endl;
   cout<< "Volume of object2" << "height("<< obj2.height<<") " <<"length("<< obj2.length <<") "<< "width("<< obj2.width <<") "<< "is " << obj2.volume()<< endl;
-
-
-
 return 0;
 }
