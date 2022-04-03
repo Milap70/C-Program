41a10de07c279205531a7a0f632cc7fefa4ee725
Milap70 <98155640+Milap70@users.noreply.github.com>
2022-04-03 10:20:07 +0530

    Update and rename main (3).cpp to Q_6.cpp

--- Assignment/Q_6.cpp
+++ Assignment/Q_6.cpp
@@ -1,10 +1,16 @@
-/******************************************************************************
+/* The given code is  The grade of a student can be based on the following criteria:
+	Attendance must be more than 50%
+	Tests scores must be more than .70
+	Total score must be more than 5600 (just go with this number)
+The grades are awarded as follows:
+	Grade is 10: if all conditions are met
+	Grade is 9: if conditions 1 and 2 are met
+	Grade is 8: if conditions 3 and 3 are met
+	Grade is 7: if conditions 1 and 3 are met
+	Grade is 6: if only one condition is met
+	Grade is 5: if none of the conditions are met
+*/
 
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 
 #include <iostream>
 
